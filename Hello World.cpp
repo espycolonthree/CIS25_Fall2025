92b754c9f81b10d2a5599fda4567f64bead8dd44
espycolonthree <espyfrench@gmail.com>
2025-08-25 16:25:58 -0400

    Week 1

---
+++ Hello World.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std; 
+int main() { 
+    cout << "Hello Inventory System!";
+    cout << "\n\tData Types:\n";
+    cout << "Size of int: " << sizeof(int) << " byte(s)\n";
+    cout << "Size of char: " << sizeof(char) << " byte(s)\n";
+    cout << "Size of float: " << sizeof(float) << " byte(s)";
+
+    return 0;
+}
